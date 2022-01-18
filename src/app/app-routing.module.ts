5adf1731ccf510d68c76d9ac8a8bf94486855173
lamine87 <laminenatidiarra@yahoo.fr>
2022-01-18 12:26:55 +0100

    Adding Formulaire and Consultation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultationComponent } from './consultation/consultation.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Consultation", component: ConsultationComponent},
+  { path: "Formulaire", component: FormulaireComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
